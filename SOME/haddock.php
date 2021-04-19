4d6c81b640062162b189e823e1b0e5f27cbad200
ShamSandra <79218160+ShamSandra@users.noreply.github.com>
2021-04-19 04:49:45 -0400

    Update haddock.php

--- SOME/haddock.php
+++ SOME/haddock.php
@@ -5,7 +5,7 @@
     $myfile=fopen("addtoCart.xml", "a") or die("Unable to find the path/folder for the accounts");
     
     if (isset($_GET["additem"])){
-        $pName="Fresh Haddock Filet";
+        $pName="Haddock";
         $pUnitPrice=6.39;
         $pQuantity=$_GET["pquantity"];
         
