55297c363631391f52a1d367c1e1fdea3051960d
ShamSandra <79218160+ShamSandra@users.noreply.github.com>
2021-04-19 04:38:46 -0400

    Update bellp.php

--- SOME/bellp.php
+++ SOME/bellp.php
@@ -5,7 +5,7 @@
     $myfile=fopen("addtoCart.xml", "a") or die("Unable to find the path/folder for the accounts");
     
     if (isset($_GET["additem"])){
-        $pName="Sweet Bell Pepper";
+        $pName="Bellpepper";
         $pUnitPrice=2.99;
         $pQuantity=$_GET["pquantity"];
         
