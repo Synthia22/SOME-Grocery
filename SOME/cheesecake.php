9bbfbf6b84d6e2c19a7198c36f2a7b9f36dcb995
ShamSandra <79218160+ShamSandra@users.noreply.github.com>
2021-04-19 02:30:28 -0400

    Update cheesecake.php

--- SOME/cheesecake.php
+++ SOME/cheesecake.php
@@ -50,6 +50,7 @@
         
         <!--Cheesecake-->
         <div class="row">
+            <form method="GET">
             <div class="column"> <br>
                 <img src="pics/cheesecake.jpg" alt="background-image" weight="300" height="300">
             </div>                
@@ -68,7 +69,8 @@
                     <label for="quantity">Quantity:</label>
                     <input type="number" id="quantity" value="quantity" name="pquantity" onclick="modPrice()" min="0"> <br>
                     <button id="additem"  name="additem" onclick="addItem()"> <i class="fas fa-shopping-cart"></i> ADD TO CART </button>
-            </div> 
+            </div>
+            </form>
         </div><br>
         <!--End of Cupcakes-->
         <!--Footer-->
