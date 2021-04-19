1733791dc4adfb42cb7c69974078d63f48a878b1
ShamSandra <79218160+ShamSandra@users.noreply.github.com>
2021-04-19 04:58:34 -0400

    Update steak.php

--- SOME/steak.php
+++ SOME/steak.php
@@ -5,7 +5,7 @@
     $myfile=fopen("addtoCart.xml", "a") or die("Unable to find the path/folder for the accounts");
     
     if (isset($_GET["additem"])){
-        $pName="Sirloin Steak";
+        $pName="Steak";
         $pUnitPrice=11.59;
         $pQuantity=$_GET["pquantity"];
         
