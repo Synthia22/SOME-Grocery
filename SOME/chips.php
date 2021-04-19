c813d8b591b3b964e9ca92e9b53b09a6225f4a23
ShamSandra <79218160+ShamSandra@users.noreply.github.com>
2021-04-19 04:44:30 -0400

    Update chips.php

--- SOME/chips.php
+++ SOME/chips.php
@@ -5,7 +5,7 @@
     $myfile=fopen("addtoCart.xml", "a") or die("Unable to find the path/folder for the accounts");
     
     if (isset($_GET["additem"])){
-        $pName="Original Chips";
+        $pName="Chips";
         $pUnitPrice=1.75;
         $pQuantity=$_GET["pquantity"];
         
