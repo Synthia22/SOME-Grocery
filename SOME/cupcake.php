ecb95e34fd815381053566dddfdf9b90c0ce8ccc
ShamSandra <79218160+ShamSandra@users.noreply.github.com>
2021-04-19 04:42:21 -0400

    Update cupcake.php

--- SOME/cupcake.php
+++ SOME/cupcake.php
@@ -5,7 +5,7 @@
     $myfile=fopen("addtoCart.xml", "a") or die("Unable to find the path/folder for the accounts");
     
     if (isset($_GET["additem"])){
-        $pName="Vanilla Cupcake";
+        $pName="Cupcake";
         $pUnitPrice=2;
         $pQuantity=$_GET["pquantity"];
         
