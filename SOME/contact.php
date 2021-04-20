88446855738dcd55fc05d77183acde0450b71656
Synthia22 <59714971+Synthia22@users.noreply.github.com>
2021-04-20 11:54:33 -0400

    Add files via upload

---
+++ SOME/contact.php
@@ -0,0 +1,52 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    //$mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'somebrandtest@gmail.com';                     //SMTP username
+    $mail->Password   = 'SomeExampleTry123@';                               //SMTP password
+            //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    $email=$_POST['email']; //getting email value from HTML form
+    $message=$_POST['comment']; //getting message value from HTML form
+
+    //Recipients
+    $mail->setFrom('somebrandtest@gmail.com', 'Some Brand');
+    $mail->addAddress($email);     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    $mail->addAttachment('logo.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'SOME BRAND-Thank you for your feedback!';
+    $mail->Body    = 'Hello! Thank you for contacting us! We will be back to you shortly!';
+   
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'An email has been sent to your inbox!';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
